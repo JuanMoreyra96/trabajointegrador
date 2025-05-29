fb0994a16e8f9a36b25626242368a2ee5e99c9d0
Tomas Moreyra <113137098+Toomaaass@users.noreply.github.com>
2025-05-29 19:54:32 -0300

    Creacion FechaHora
    Falta terminar

---
+++ fechaHora.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "fechaHora.h"
+
+FechaHora::FechaHora(){
+    setDia(0);
+    setMes(0);
+    setAnio(0);
+    setHora(0);
+    setMinuto(0);
+}
+
+FechaHora::FechaHora(int dia, int mes, int anio, int hora, int minuto){
+    setDia(dia);
+    setMes (mes);
+    setAnio (anio);
+    setHora (hora);
+    setMinuto (minuto);
+}
+
+int FechaHora::getDia(){
+    return _dia;
+}
+int FechaHora::getMes(){
+    return _mes;
+}
+int FechaHora::getAnio(){
+    return _anio;
+}
+int FechaHora::getHora(){
+    return _hora;
+}
+int FechaHora::getMinuto(){
+    return _minuto;
+}
+
+void FechaHora::setDia(int dia){
+    _dia=dia;
+}
+void FechaHora::setMes (int mes){
+    _mes=mes;
+}
+void FechaHora::setAnio (int anio){
+    _anio=anio;
+}
+void FechaHora::setHora (int hora){
+    _hora=hora;
+}
+void FechaHora::setMinuto (int minuto){
+    _minuto=minuto;
+}
+
+void FechaHora::Cargar(int dia, int mes, int anio, int hora, int minuto){
+    setDia(dia);
+    setMes(mes);
+    setAnio(anio);
+    setHora(hora);
+    setMinuto(minuto);
+}
+
+void FechaHora::Mostrar(){
+    std::cout << "La hora es: " << _hora << ":" << _minuto <<  endl;
+    std::cout << "La fecha es: " << _dia << "/" << _mes << "/" << _anio << endl;
+}
---
+++ fechaHora.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class FechaHora{
+private:
+    int _dia;
+    int _mes;
+    int _anio;
+    int _hora;
+    int _minuto;
+
+public:
+    FechaHora();
+    FechaHora(int dia, int mes, int anio, int hora, int minuto);
+
+    int getDia();
+    int getMes();
+    int getAnio();
+    int getHora();
+    int getMinuto();
+
+    void setDia(int dia);
+    void setMes (int mes);
+    void setAnio (int anio);
+    void setHora (int hora);
+    void setMinuto (int minuto);
+
+    void Cargar();
+    void Mostrar();
+
+};
