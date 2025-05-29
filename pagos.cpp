128e7d20a61cfcfdb60a9d10319e7d9063df39a6
Tomas Moreyra <113137098+Toomaaass@users.noreply.github.com>
2025-05-29 14:44:58 -0300

    Pequeñas correcciones
    Se corrigieron pequeños errores

--- pagos.cpp
+++ pagos.cpp
@@ -4,25 +4,25 @@
 //# include "archivoPagos.h"
 //# include Fecha.h
 
-void Pagos::setIdPago(int idPago){_idPago=idPago};
-void Pagos::setIdReserva(int idReserva){_idReserva=idReserva};
-void Pagos::setImporte(float importe){_importe=importe};
-void Pagos::setFecha(Fecha fecha){_fecha=fecha};
+void Pagos::setIdPago(int idPago){_idPago=idPago;}
+void Pagos::setIdReserva(int idReserva){_idReserva=idReserva;}
+void Pagos::setImporte(float importe){_importe=importe;}
+//void Pagos::setFecha(Fecha fecha){_fecha=fecha};
 
-int Pagos::getIdPago(){return _idPago};
-int Pagos::getIdReserva(){return _idReserva};
-float Pagos::getImporte(){return _importe};
-Fecha Pagos::getFecha(){return _fecha};
+int Pagos::getIdPago(){return _idPago;}
+int Pagos::getIdReserva(){return _idReserva;}
+float Pagos::getImporte(){return _importe;}
+//Fecha Pagos::getFecha(){return _fecha};
 
-void cargar(int idPago, int idReserva, float importe, Fecha fecha) {
+void cargar(int idPago, int idReserva, float importe/*, Fecha fecha*/) {
         setIdPago(idPago);
         setIdReserva(idReserva);
         setImporte(importe);
-        setFecha(fecha);
+        //setFecha(fecha);
     }
 void Mostrar(){
-    cout "ID Pago: " _idPago;
-    cout "ID Reserva: " _idReserva;
-    cout "Importe: " _importe;
-    cout "Fecha del pago: " _fecha;
+    std::cout << "ID Pago: " << _idPago << endl;
+    std::cout << "ID Reserva: " << _idReserva << endl;
+    std::cout << "Importe: " << _importe << endl;
+    //std::cout << "Fecha del pago: " << _fecha << endl;
 };
--- pagos.h
+++ pagos.h
@@ -5,21 +5,21 @@ private:
     int _idPago;
     int _idReserva;
     float _importe;
-    Fecha _fecha;
+    //Fecha _fecha;
 
 public:
-    Pagos(int idPago, int idReserva, float importe, Fecha fecha);
+    Pagos(int idPago, int idReserva, float importe/*, Fecha fecha*/);
     Pagos ();
 
     void setIdPago(int idPago);
     void setIdReserva(int idReserva);
     void setImporte(float importe);
-    void setFecha(Fecha fecha);
+    //void setFecha(Fecha fecha);
 
     int getIdPago();
     int getIdReserva();
     float getImporte();
-    Fecha getFecha();
+    //Fecha getFecha();
 
     bool cargar();
     void costrar();
